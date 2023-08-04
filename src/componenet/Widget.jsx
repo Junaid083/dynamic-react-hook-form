044a77a98ba58e62a216ac3a9bba9b7693347f64
junaid083 <muhammad.junaid@amroodlabs.com>
2023-08-04 18:43:13 +0500

    Widget Updated

--- src/componenet/Widget.jsx
+++ src/componenet/Widget.jsx
@@ -2,9 +2,11 @@ const Widget = ({ title, description, right, children }) => {
   return (
     <div className="w-full p-4 mb-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
       {(title || description || right) && (
-        <div className="flex flex-row items-center justify-between mb-6">
+        <div className="flex  text-center items-center justify-center mb-6">
           <div className="flex flex-col">
-            <div className="text-sm font-light text-gray-500">{title}</div>
+            <div className="text-lg text-center font-light text-gray-400">
+              {title}
+            </div>
 
             <div className="text-sm font-bold">{description}</div>
           </div>

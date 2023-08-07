daedffa70af021ecc7a448669fd705d64b103bd3
junaid083 <muhammad.junaid@amroodlabs.com>
2023-08-07 16:19:30 +0500

    Extra Data removed and created seperate componenet

--- src/componenet/Form.jsx
+++ src/componenet/Form.jsx
@@ -1,178 +1,27 @@
 import React from "react";
 import Widget from "./Widget";
 import { useForm, Controller } from "react-hook-form";
-
-const Form = () => {
+import Input from "./Input";
+const Form = ({ dynamicForm, onSubmit }) => {
   const {
     control,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const dynamicForm = {
-    firstName: {
-      label: "First Name",
-      type: "text",
-      placeholder: "Enter your first name",
-      defaultValue: "",
-      rules: {
-        required: true,
-      },
-    },
-    lastName: {
-      label: "Last Name",
-      type: "text",
-      placeholder: "Enter your last name",
-      defaultValue: "",
-      rules: {
-        required: true,
-      },
-    },
-    gender: {
-      label: "Gender",
-      type: "radio",
-      options: ["Male", "Female"],
-      defaultValue: "",
-      rules: {
-        required: true,
-      },
-    },
-    profession: {
-      label: "Profession",
-      type: "dropdown",
-      options: ["Front-end Developer", "Back-end Developer", "Devops Engineer"],
-      defaultValue: "",
-      rules: {
-        required: true,
-      },
-    },
 
-    social: [
-      {
-        label: "Facebook",
-        type: "text",
-        defaultValue: "",
-        rules: {
-          required: true,
-        },
-      },
-      {
-        label: "Twitter",
-        type: "text",
-        defaultValue: "",
-        rules: {
-          required: true,
-        },
-      },
-    ],
-    contact: [
-      {
-        label: "Contact 1",
-        type: "number",
-        defaultValue: "",
-        rules: {
-          required: true,
-        },
-      },
-      {
-        label: "Contact 2",
-        type: "number",
-        defaultValue: "",
-        rules: {
-          required: true,
-        },
-      },
-    ],
-  };
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
-  const Input = ({ value, onChange, type, options, ...rest }) => {
-    switch (type) {
-      case "text":
-        return (
-          <input
-            className={`form-input block w-96 h-10 px-4 mt-1 mb-2 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-700 flex-grow-1 focus:border-blue-500 focus:ring-0 sm:text-sm rounded-md `}
-            type="text"
-            value={value}
-            onChange={onChange}
-            {...rest}
-          />
-        );
-      case "number":
-        return (
-          <input
-            className={`form-input block w-96 h-10 px-4 mt-1 mb-2 border-gray-300 bg-white dark:bg-gray-800 dark:border-gray-700 flex-grow-1 focus:border-blue-500 focus:ring-0 sm:text-sm rounded-md `}
-            type="number"
-            value={value}
-            onChange={onChange}
-            {...rest}
-          />
-        );
-      case "radio":
-        return options.map((option, index) => (
-          <label className="px-2" key={index}>
-            <input
-              className="w-4 h-4text-blue-600 border-gray-300 form-radio focus:ring-blue-500"
-              type="radio"
-              value={option}
-              checked={value === option}
-              onChange={(e) => onChange(e.target.value)}
-              {...rest}
-            />
-            {option}
-          </label>
-        ));
-      case "dropdown":
-        return (
-          <select
-            className={`block w-full h-8 mb-4 border-gray-300 text-black dark:bg-gray-800 dark:border-gray-700 form-select focus:ring-blue-500 focus:border-blue-500 focus:ring-0 sm:text-sm rounded-md`}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            {...rest}
-          >
-            <option value="">Select an option</option>
-            {options.map((option, index) => (
-              <option key={index} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        );
-      case "checkbox":
-        return (
-          <label>
-            <input
-              type="checkbox"
-              checked={value}
-              className="w-4 h-4 mb-4 text-blue-600 border-gray-300 rounded form-checkbox focus:ring-blue-500"
-              onChange={(e) => onChange(e.target.checked)}
-              {...rest}
-            />
-            {rest.checkboxLabel}
-          </label>
-        );
-      default:
-        return null;
-    }
-  };
   const Error = ({ children }) => <p style={{ color: "red" }}>{children}</p>;
   return (
     <Widget title="React Hook Form" description={<span></span>}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* {formInputs} */}
-
         {Object.keys(dynamicForm).map((e) => {
           const { rules, defaultValue, label, type, options } = dynamicForm[e];
 
           if (Array.isArray(dynamicForm[e])) {
-            // For array fields like "social" and "contact"
             return (
               <section key={e}>
                 {dynamicForm[e].map((item, index) => {
                   const { label, type, defaultValue, rules } = item;
-                  const fieldName = `${e}[${index}].${label}`; // Create a field name like "social[0]" or "contact[1]"
+                  const fieldName = `${e}[${index}].${label}`;
 
                   return (
                     <div key={fieldName}>
